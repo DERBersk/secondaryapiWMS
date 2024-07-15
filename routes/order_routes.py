93173d17d04b237d991d61432045ab791cd2ecc8
Dominik Eitner <76207875+DERBersk@users.noreply.github.com>
2024-07-15 13:23:49 +0200

    New Order Route

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -5,9 +5,17 @@ from models.order import Order
 o_bp = Blueprint('order', __name__, url_prefix='/order')
 
 ###################################################
-# Get for multiple orders
+# Get for multiple Orders
 ###################################################
 @o_bp.route('/', methods=['GET'])
 def get_orders():
     orders = Order.query.all()
+    return jsonify([o.serialize() for o in orders])
+
+###################################################
+# Get for a single Orders
+###################################################
+@o_bp.route('/<int:supplier_id>', methods=['GET'])
+def get_orders_from_supplier(supplier_id):
+    orders = Order.query.filter(Order.supplier_id == supplier_id).all()
     return jsonify([o.serialize() for o in orders])
